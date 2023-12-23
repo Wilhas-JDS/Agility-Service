b900be8dfc96873e7a55a369ddf42c8df0cbb5ba
Wilhas-JDS <wilhas125@hotmail.com>
2023-12-22 23:48:02 -0300

    Criação das letas Cigarros e Wys

--- scr/pages/precoErrado/PreçoErrado.js
+++ scr/pages/precoErrado/PreçoErrado.js
@@ -1,62 +1,136 @@
-import React, { useState } from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Text, Image, Touchable, TouchableOpacity } from "react-native";
 import RNPickerSelect from "react-native-picker-select";
 
-export default function PreçoErrado() {
-  const [selectedItem, setSelectedItem] = useState(null);
+import Menu from '../../pages/menu/Menu';
 
-  const placeholder = {
-    label: "Selecione uma opção...",
+export default function Troco() {
+  const [selectedValor, setSelectedValor] = useState(null);
+  const [selectedTipo, setSelectedTipo] = useState(null);
+  const [caixa, setCaixa] = useState("14");
+
+  const placeholderValor = {
+    label: "Selecione o Valor",
+    value: null,
+    color: "#9EA0A4",
+  };
+  const placeholderTipo = {
+    label: "Selecione o tipo",
     value: null,
     color: "#9EA0A4",
   };
 
-  const data = [
-    { label: "Opção 1", value: "option1" },
-    { label: "Opção 2", value: "option2" },
-    { label: "Opção 3", value: "option3" },
+  const valor = [
+    { label: "R$ 5 (2x 2 + 1)", value: "R$ 5 (2x 2 + 1)" },
+    { label: "R$ 10 (2x 5)", value: "R$ 10 (2x 5)" },
+    { label: "R$ 10 (5x 2)", value: "R$ 10 (5x 2)" },
+    { label: "R$ 20 (2X 10)", value: "R$ 20 (2X 10)" },
+    { label: "R$ 20 (1X R$10 + 2x R$5)", value: "R$ 20 (1X R$10 + 2x R$5)" },
+    { label: "R$ 50 (2X R$20 + 1x R$10)", value: "R$ 50 (2X R$20 + 1x R$10)" },
+    { label: "R$ 50 (1X R$20 + 3x R$10)", value: "R$ 50 (1X R$20 + 3x R$10)" },
+    { label: "R$ 100 (2X R$50)", value: "R$ 100 (2X R$50)" },
+    { label: "R$ 100 (1X R$50 2x 20 1x 10)", value: "R$ 100 (1X R$50 2x 20 1x 10)" },
+    { label: "R$ 200 (1X R$100 2x 50)", value: "R$ 200 (1X R$100 2x 50)" },
+    { label: "R$ 200 (1X R$100 2x 20 2x 5)", value: "R$ 200 (1X R$100 2x 20 2x 5)" },
+
+  ];
+  const tipo = [
+    { label: "R$ 5", value: "R$ 5" },
+    { label: "R$ 10", value: "R$ 10" },
+    { label: "R$ 20", value: "R$ 20" },
+    { label: "R$ 50", value: "R$ 50" },
+    { label: "R$ 100", value: "R$ 100" },
+    { label: "R$ 200", value: "R$ 200" },
   ];
 
   return (
+
     <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={require('../../../assets/logoSub.png')} />
+        <Text style={{fontSize: 20, fontWeight: 'bold'}}>Nº CAIXA:{caixa}</Text>
+      </View>
+
       <RNPickerSelect
-        placeholder={placeholder}
-        items={data}
-        onValueChange={(value) => setSelectedItem(value)}
-        value={selectedItem}
-        style={pickerSelectStyles}
+        placeholder={placeholderValor}
+        items={valor}
+        onValueChange={(value) => setSelectedValor(value)}
+        value={selectedValor}
+        style={styles.picker}
       />
+      <RNPickerSelect
+        placeholder={placeholderTipo}
+        items={tipo}
+        onValueChange={(value) => setSelectedTipo(value)}
+        value={selectedTipo}
+        style={styles.picker}
+      />
+      
+      <TouchableOpacity style={styles.botaoSoliAt}>
+        <Text style={styles.botaoCbTexto}>
+          Solicitar Atendimento
+        </Text>
+      </TouchableOpacity>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
+    backgroundColor: '#209A57',
+    padding: 20,
 
-const pickerSelectStyles = StyleSheet.create({
-  inputIOS: {
-    fontSize: 16,
-    paddingVertical: 12,
-    paddingHorizontal: 10,
-    borderWidth: 1,
-    borderColor: "gray",
-    borderRadius: 4,
-    color: "black",
-    paddingRight: 30, // to ensure the text is never behind the icon
   },
-  inputAndroid: {
-    fontSize: 16,
-    paddingHorizontal: 10,
-    paddingVertical: 8,
-    borderWidth: 0.5,
-    borderColor: "purple",
-    borderRadius: 8,
-    color: "black",
-    paddingRight: 30, // to ensure the text is never behind the icon
+  texto: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  botaoCb: {
+    flex: 0,
+    fontSize: 30,
+    alignItems: "center",
+    marginTop: 20,
+    backgroundColor: '#2A784D',
+    borderRadius: 50,
+    width: 'auto',
+  },
+  botaoCbTexto: {
+    fontSize: 15,
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  header: {
+    flexDirection: 'column',
+    alignItems: "center",
+    height: 250,
+    width: 'auto',
+    padding: 50,
+
+  },
+  picker: {
+    fontSize: 20,
+    height: 50,
+    backgroundColor: '#fff',
+    marginBottom: 150,
+    marginTop: 110,
+    paddingHorizontal: 5,
+  },
+  
+  botaoSoliAt: {
+    backgroundColor: '#ca6500',
+    padding: 25,
+    borderRadius: 50,
+    alignSelf: 'center', // Alinha o botão à direita
+    marginTop: 500,
+
+  },
+  botaoTextoSoliAt: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 30,
+    fontStyle: 'italic',
   },
 });
--- scr/pages/whisky/Whisky.js
+++ scr/pages/whisky/Whisky.js
@@ -1,14 +1,17 @@
-import React, { useState } from "react";
-import { View, StyleSheet, Text, Image } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View, StyleSheet, Text, Image, Touchable, TouchableOpacity } from "react-native";
 import RNPickerSelect from "react-native-picker-select";
 
+import Menu from '../../pages/menu/Menu';
 
-export default function Whisky() {
-  const [selectedValor, setSelectedValor] = useState(null);
+export default function Troco() {
+  const [selectedMarca, setSelectedMarca] = useState(null);
   const [selectedTipo, setSelectedTipo] = useState(null);
+  const [selectedQtd, setSelectedQtd] = useState(null);
+  const [caixa, setCaixa] = useState("14");
 
-  const placeholderValor = {
-    label: "Selecione o Valor",
+  const placeholderMarca = {
+    label: "Selecione a marca desejada",
     value: null,
     color: "#9EA0A4",
   };
@@ -17,54 +20,75 @@ export default function Whisky() {
     value: null,
     color: "#9EA0A4",
   };
+  const placeholderQtd = {
+    label: "Selecione a quantidade",
+    value: null,
+    color: "#9EA0A4",
+  };
 
-  const valor = [
-    { label: "R$ 5 (2x 2 + 1)", value: "R$ 5 (2x 2 + 1)" },
-    { label: "R$ 10 (2x 5)", value: "R$ 10 (2x 5)" },
-    { label: "R$ 10 (5x 2)", value: "R$ 10 (5x 2)" },
-    { label: "R$ 20 (2X 10)", value: "R$ 20 (2X 10)" },
-    { label: "R$ 20 (1X R$10 + 2x R$5)", value: "R$ 20 (1X R$10 + 2x R$5)" },
-    { label: "R$ 50 (2X R$20 + 1x R$10)", value: "R$ 50 (2X R$20 + 1x R$10)" },
-    { label: "R$ 50 (1X R$20 + 3x R$10)", value: "R$ 50 (1X R$20 + 3x R$10)" },
-    { label: "R$ 100 (2X R$50)", value: "R$ 100 (2X R$50)" },
-    { label: "R$ 100 (1X R$50 2x 20 1x 10)", value: "R$ 100 (1X R$50 2x 20 1x 10)" },
-    { label: "R$ 200 (1X R$100 2x 50)", value: "R$ 200 (1X R$100 2x 50)" },
-    { label: "R$ 200 (1X R$100 2x 20 2x 5)", value: "R$ 200 (1X R$100 2x 20 2x 5)" },
+
+  const marca = [
+    { label: "Lucky Strike", value: "Lucky Strike" },
+    { label: "Marlboro", value: "Marlboro" },
+    { label: "Dunhill", value: "Dunhill" },
+    { label: "Camel", value: "Camel" },
+    { label: "Parliament", value: "Parliament" },
 
   ];
   const tipo = [
-    { label: "R$ 5", value: "R$ 5" },
-    { label: "R$ 10", value: "R$ 10" },
-    { label: "R$ 20", value: "R$ 20" },
-    { label: "R$ 50", value: "R$ 50" },
-    { label: "R$ 100", value: "R$ 100" },
-    { label: "R$ 200", value: "R$ 200" },
+    { label: 'Maço', value: "Maço" },
+    { label: "Box", value: "Box" },
   ];
+  const qtd = [
+    { label: "1", value: "1" },
+    { label: "2", value: "2" },
+    { label: "3", value: "3" },
+    { label: "4", value: "4" },
+    { label: "5", value: "5" },
+    { label: "6", value: "6" },
+    { label: "7", value: "7" },
+    { label: "8", value: "8" },
+    { label: "9", value: "9" },
+    { label: "10", value: "10" },
+
+  ];
+
 
   return (
 
     <View style={styles.container}>
-      <View style={styles.imageStyles}>
+      <View style={styles.header}>
         <Image source={require('../../../assets/logoSub.png')} />
-        <Text>
-          Nº Caixa
-        </Text>
+        <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Nº CAIXA:{caixa}</Text>
       </View>
 
       <RNPickerSelect
-  placeholder={placeholderValor}
-  items={valor}
-  onValueChange={(value) => setSelectedValor(value)}
-  value={selectedValor}
-  style={pickerSelectStyles}
-/>
-<RNPickerSelect
-  placeholder={placeholderTipo}
-  items={tipo}
-  onValueChange={(value) => setSelectedTipo(value)}
-  value={selectedTipo}
-  style={pickerSelectStyles}
-/>
+        placeholder={placeholderMarca}
+        items={marca}
+        onValueChange={(value) => setSelectedMarca(value)}
+        value={selectedMarca}
+        style={styles.picker}
+      />
+      <RNPickerSelect
+        placeholder={placeholderTipo}
+        items={tipo}
+        onValueChange={(value) => setSelectedTipo(value)}
+        value={selectedTipo}
+        style={styles.picker}
+      />
+      <RNPickerSelect
+        placeholder={placeholderQtd}
+        items={qtd}
+        onValueChange={(value) => setSelectedQtd(value)}
+        value={placeholderQtd}
+        style={styles.picker}
+      />
+
+      <TouchableOpacity style={styles.botaoSoliAt}>
+        <Text style={styles.botaoCbTexto}>
+          Solicitar Atendimento
+        </Text>
+      </TouchableOpacity>
     </View>
 
   );
@@ -73,39 +97,58 @@ export default function Whisky() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#209A57',
+    padding: 20,
+
+  },
+  texto: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  botaoCb: {
+    flex: 0,
+    fontSize: 30,
+    alignItems: "center",
+    marginTop: 20,
     backgroundColor: '#2A784D',
-
+    borderRadius: 50,
+    width: 'auto',
   },
-});
-
-const pickerSelectStyles = StyleSheet.create({
-  inputIOS: {
-    fontSize: 16,
-    paddingVertical: 12,
-    paddingHorizontal: 10,
-    borderWidth: 1,
-    borderColor: "gray",
-    borderRadius: 4,
-    color: "black",
-    paddingRight: 30, // to ensure the text is never behind the icon
+  botaoCbTexto: {
+    fontSize: 15,
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 30,
   },
-  inputAndroid: {
-    fontSize: 16,
-    paddingHorizontal: 10,
-    paddingVertical: 8,
-    borderWidth: 0.5,
-    borderColor: "purple",
-    borderRadius: 8,
-    color: "black",
-    paddingRight: 30, // to ensure the text is never behind the icon
-  },
-
-  imageStyles: {
-    flexDirection: 'row',
+  header: {
+    flexDirection: 'column',
     alignItems: "center",
-    justifyContent: "space-between",
-    marginBottom: 20,
-    justifyContent: "center",
-    alignItems: "center",
-  }
+    height: 250,
+    width: 'auto',
+    padding: 50,
+
+  },
+  picker: {
+    fontSize: 20,
+    height: 50,
+    backgroundColor: '#fff',
+    marginBottom: 150,
+    marginTop: 110,
+    paddingHorizontal: 5,
+  },
+
+  botaoSoliAt: {
+    backgroundColor: '#ca6500',
+    padding: 25,
+    borderRadius: 50,
+    alignSelf: 'center', // Alinha o botão à direita
+    marginTop: 500,
+
+  },
+  botaoTextoSoliAt: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 30,
+    fontStyle: 'italic',
+  },
 });
